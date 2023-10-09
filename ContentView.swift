56b3e1742f6d32971f0ea482c665eb7d23483f8c
Himanshu Pahwa <56366231+himanshupahwaa@users.noreply.github.com>
2023-10-09 08:12:16 -0400

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,95 @@
+//
+//  ContentView.swift
+//  War Card Game
+//
+//  Created by Himanshu Pahwa on 10/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var playerCard = "card7"
+    @State var cpuCard = "card13"
+    @State var playerScore = 0
+    @State var cpuScore = 0
+    
+    var body: some View {
+        
+        ZStack{
+            
+            Image("background-plain")
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack{
+                Spacer()
+                Image("logo")
+                
+                Spacer()
+                HStack{
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                
+                Spacer()
+                Button {
+                    Deal()
+                } label: {
+                    Image("button")
+                }
+
+                
+                Spacer()
+                HStack{
+                    Spacer()
+                    VStack{
+                        Text("Player")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                    }
+                    
+                    Spacer()
+                    VStack{
+                        Text("CPU")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                }
+                .foregroundColor(.white)
+                Spacer()
+            }
+        }
+        
+        
+    }
+    
+    func Deal() {
+        var pcv = Int.random(in: 2...14)
+        var ccv = Int.random(in: 2...14)
+        playerCard = "card" + String(pcv)
+        cpuCard = "card" + String(ccv)
+        
+        if pcv > ccv{
+            playerScore += 1
+        }
+        else if pcv < ccv{
+            cpuScore += 1
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
